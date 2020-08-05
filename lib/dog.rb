c17f9994a6b7494d163bd5eceed1eb6a5e967953
kain <kaindoneson@gmail.com>
2020-08-04 21:14:33 -0400

    more labs

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+class Dog
+    
+    @@all = [];
+
+    attr_accessor :name;
+
+    def initialize (name)
+        @name = name;
+        self.save;
+    end
+
+    def self.all
+        @@all;
+    end
+    # omg i keep doing bracket notation for like dog[name] cuz JS and get super confused by the no method error lol!!
+    def self.print_all
+        @@all.each{|dog| puts dog.name} # we can also use self.all but I like @@all since were in the scope to do so.
+    end
+    # i think this is a local method and not a class mettod because we are invoking it during the init constructor
+    # and I think self becomes the instance we are creating but im a bit confused.
+    def save
+        @@all << self;
+    end
+
+    def self.clear_all
+        self.all.clear;
+    end
+
+end
