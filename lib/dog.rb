7efddcf1d1f9e2aabe1c21999b3cf1346425bfed
kain <kaindoneson@gmail.com>
2020-08-05 08:35:19 -0400

    I couldnt sleep so I did more reseatch on scope with self. What I now realize is that in a class method u can access @@all with self.all but in a standard method being defined in the class you need to use self.class.all. I changed @all to self.class.all in the #save method and also swapped @all for the self.all in the self.print_all method as well to illustrate this!

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,12 +14,12 @@ class Dog
     end
     # omg i keep doing bracket notation for like dog[name] cuz JS and get super confused by the no method error lol!!
     def self.print_all
-        @@all.each{|dog| puts dog.name} # we can also use self.all but I like @@all since were in the scope to do so.
+        self.all.each{|dog| puts dog.name} # we can also use self.class.all but I like @@all since were in the scope to do so.
     end
     # i think this is a local method and not a class mettod because we are invoking it during the init constructor
     # and I think self becomes the instance we are creating but im a bit confused.
     def save
-        @@all << self;
+        self.class.all << self;
     end
 
     def self.clear_all
@@ -30,3 +30,6 @@ end
 # from the quizz seem relevent to my confusion.
 # In a Car class, in order to access Car.all in #initialize, you would need to use self.class.all, 
 #and then invoke the Car.all method.
+
+# update! so it apears that calling self in the fn def in the class def refers to the class but once u go inside a class def
+# the self is refering to the new instance and should be accessed using self.class.all!
